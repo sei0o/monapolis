d46cdfc3a2a3e56b6fb3764d4e933c395df9512e
sei0o <sei0o@live.jp>
2015-10-16 10:42:02 +0900

    set up database

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'active_record'
+require 'yaml'
+
+db_config = YAML.load_file "database.yml"
+ActiveRecord::Base.establish_connection db_config["development"]
 
 class Monapolis < Sinatra::Base
   configure :development do
